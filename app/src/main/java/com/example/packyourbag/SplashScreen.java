f2fba3ed9f5d859e3f53c824706f841c280fdcb0
salmansami095 <salmansami095@gmail.com>
2023-12-04 00:34:37 +0500

    First commit

--- app/src/main/java/com/example/packyourbag/SplashScreen.java
+++ app/src/main/java/com/example/packyourbag/SplashScreen.java
@@ -1,11 +1,11 @@
 package com.example.packyourbag;
 
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashScreen extends AppCompatActivity {
 
